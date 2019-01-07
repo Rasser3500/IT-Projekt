2388441c3c7ccfa8f7e929d068eeca6fb5b2cc40
rasmu <rasmu@10.131.204.189>
2019-01-07 13:08:42 +0100

    Request to give Gogthegog a set in the coucil

--- Eksamens_Project/src/eksamens_project/Eksamens_Project.java
+++ Eksamens_Project/src/eksamens_project/Eksamens_Project.java
@@ -17,5 +17,5 @@ public class Eksamens_Project {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    //Request to give Gogthegog a set in the coucil
 }
