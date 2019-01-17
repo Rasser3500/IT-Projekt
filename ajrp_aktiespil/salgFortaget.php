e95cadb7b64f6c6443ae6a9b560c2f8ec08bc32b
rasser3500 <rasser3500@gmail.com>
2019-01-17 08:33:20 +0100

    blessed

--- ajrp_aktiespil/salgFortaget.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    include "database.php";
-    $connect = getConnectionAndCreateAll(); //genopretter forbindelse her... af en eller anden grund mistede jeg den...
-    sellAktie($connect, $_SESSION['user_id'], $_GET['aktie_id'], $_GET['pris'], $_GET['antal']);
-
-    echo "Tillykke du har lavet et SALG!";
-    echo "<a href='brugerSide.php'><button>Gå tilbage til brugerside</button></a>";
-?>
